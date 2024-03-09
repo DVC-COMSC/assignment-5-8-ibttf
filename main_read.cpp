a2067651ab5d272848179cb673b4d327a0aa92d6
roylee0912 <leeroy0912@gmail.com>
2024-03-08 16:36:04 -0800

    done

--- main_read.cpp
+++ main_read.cpp
@@ -2,9 +2,22 @@
 #include <fstream>
 using namespace std;
 
-int main()
-{
-	/***************************************************
-	 * Code your program here
-	 ************************************************** */
+int main() {
+	int N;
+	string name;
+	int score1, score2;
+
+	ifstream myfile;
+	myfile.open ("students.txt");
+
+	myfile >> N;
+
+	for(int i = 0; i < N; i++) {
+		myfile >> name >> score1 >> score2;
+		cout << name << " " << score1 << " " << score2 << "\n";
+	}
+
+	myfile.close();
+
+	return 0;
 }
--- main_write.cpp
+++ main_write.cpp
@@ -2,9 +2,31 @@
 #include <fstream>
 using namespace std;
 
-int main()
-{
-   /***************************************************
-    * Code your program here
-    ************************************************** */
+int main() {
+    int N;
+    string name;
+    int score1, score2;
+
+    cout << "Enter the number of students: ";
+    cin >> N;
+
+    ofstream myfile;
+    myfile.open ("students.txt");
+
+    myfile << N << "\n";
+
+    for(int i = 0; i < N; i++) {
+        cout << "Enter the student's name: ";
+        cin >> name;
+        cout << "Enter the first score: ";
+        cin >> score1;
+        cout << "Enter the second score: ";
+        cin >> score2;
+
+        myfile << name << " " << score1 << " " << score2 << "\n";
+    }
+
+    myfile.close();
+
+    return 0;
 }
